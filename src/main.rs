ed4e03345040744c86af1b255fe4d3a0c91a6264
Denis Soke <sokedenis@gmail.com>
2025-03-15 12:09:32 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-/*
-    Command-line shell:
-    1.Reads user input
-    2.Supports commands:
-        - cd: Changes the current directory.
-        - echo: Joins arguments with spaces and print them.
-        - pwd
-        - exit
-        - ls
-        - cat
-        - cp
-        - mv
-        - rm
-        - mkdir
-        - whoami
-        - clear
-        -  help: To see all available commands.
-    3.Runs in a loop until the user exits.
-*/
 use std::env;
 use std::fs;
 use std::io::{self, Write};
@@ -24,8 +5,11 @@ use std::path::Path;
 use std::process;
 
 fn main() {
-    println!(" --------- Custom Shell --------- ");
+    println!(" #################### Custom Shell #################### ");
     println!(" --- To see all available commands, type: help  ---");
+
+    // Store command history
+    let mut history: Vec<String> = Vec::new();
 
     loop {
         print!("myshell> ");
@@ -40,6 +24,9 @@ fn main() {
         if input.is_empty() {
             continue;
         }
+
+        //Add to history before processing
+        history.push(input.to_string());
 
         let mut parts = input.split_whitespace();
         let command = parts.next().unwrap_or("");
@@ -91,6 +78,87 @@ fn main() {
                     }
                 }
             }
+            "cp" => {
+                if args.len() < 2 {
+                    println!("cp: please provide source and destination");
+                } else {
+                    let src = args[0];
+                    let dest = args[1];
+                    match fs::copy(src, dest) {
+                        Ok(_) => println!("Copied {} to {}", src, dest),
+                        Err(e) => println!("cp: error: {}", e),
+                    }
+                }
+            }
+            "mv" => {
+                if args.len() < 2 {
+                    println!("mv: please provide source and destination");
+                } else {
+                    let src = args[0];
+                    let dest = args[1];
+                    match fs::rename(src, dest) {
+                        Ok(_) => println!("Moved {} to {}", src, dest),
+                        Err(e) => println!("mv: error: {}", e),
+                    }
+                }
+            }
+            "rm" => {
+                if args.is_empty() {
+                    println!("rm: please provide a file or directory");
+                } else {
+                    let path = Path::new(args[0]);
+                    if path.is_dir() {
+                        match fs::remove_dir_all(path) {
+                            Ok(_) => println!("Removed directory {}", args[0]),
+                            Err(e) => println!("rm: error: {}", e),
+                        }
+                    } else {
+                        match fs::remove_file(path) {
+                            Ok(_) => println!("Removed file {}", args[0]),
+                            Err(e) => println!("rm: error: {}", e),
+                        }
+                    }
+                }
+            }
+            "mkdir" => {
+                if args.is_empty() {
+                    println!("mkdir: please provide a directory name");
+                } else {
+                    let path = args[0];
+                    match fs::create_dir(path) {
+                        Ok(_) => println!("Created directory {}", path),
+                        Err(e) => println!("mkdir: error {}", e),
+                    }
+                }
+            }
+            "touch" => {
+                if args.is_empty() {
+                    println!("touch: please provide a file name");
+                } else {
+                    let path = args[0];
+                    match fs::File::create(path) {
+                        Ok(_) => println!("Crated file {}", path),
+                        Err(e) => println!("touch: error: {}", e),
+                    }
+                }
+            }
+            "whoami" => match env::var("USER").or_else(|_| env::var("USERNAME")) {
+                Ok(user) => println!("{}", user),
+                Err(e) => println!("Whoami: error: {}", e),
+            },
+            "clear" => {
+                println!("\x1B[2J\x1B[H");
+                io::stdout().flush().unwrap();
+            }
+            "history" => {
+                if history.is_empty() {
+                    println!("No command history yet.");
+                } else {
+                    for (i, cmd) in history.iter().enumerate() {
+                        println!("{} : {}", i + 1, cmd);
+                    }
+                }
+            }
             "help" => {
                 println!("Available commands: ");
                 println!(" cd <dir>             - Change the current directory");
@@ -103,8 +171,10 @@ fn main() {
                 println!(" mv <src> <dest>      - Move or rename a file");
                 println!(" rm <path>            - Remove a file or directory ");
                 println!(" mkdir <dir>          - Create a new directory");
+                println!(" touch <file>         - Create an empty file");
                 println!(" whoami               - Print the current username");
                 println!(" clear                - Clear the terminal screen");
+                println!(" history              - Show command history");
                 println!(" help                 - Show this help message");
             }
             _ => {
